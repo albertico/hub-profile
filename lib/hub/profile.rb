13c8cd3ce04e6cd58158a98a509bb912df4f7894
Alberto Colon Viera <aacv@alberti.co>
2020-08-08 23:59:52 -0400

    Adding 'hub-profile version' command.

--- lib/hub/profile.rb
+++ lib/hub/profile.rb
@@ -1,10 +1,28 @@
 # frozen_string_literal: true
 
 require "hub/profile/version"
+require "dry/cli"
+require "mkmf"
 
 module Hub
   module Profile
-    class Error < StandardError; end
-    # Your code goes here...
+    # Main module containing CLI commands.
+    module CLI
+      extend Dry::CLI::Registry
+
+      # Version command.
+      class Version < Dry::CLI::Command
+        desc "Print version"
+
+        def call(*)
+          if MakeMakefile.find_executable0 "hub"
+            Kernel.system "hub version"
+          else
+            puts "Error: hub not found"
+          end
+          puts "hub-profile version #{Hub::Profile::VERSION}"
+        end
+      end
+    end
   end
 end
