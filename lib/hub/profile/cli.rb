0933a2976bcc858115a81e23758c2db8c1437e68
Alberto Colon Viera <aacv@alberti.co>
2020-08-10 21:26:05 -0400

    Adding 'hub-profile list' command.

--- lib/hub/profile/cli.rb
+++ lib/hub/profile/cli.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require "hub/profile/version"
+require "hub/profile/util"
 require "dry/cli"
 require "mkmf"
 
@@ -9,6 +10,26 @@ module Hub
     # Main module containing CLI commands.
     module CLI
       extend Dry::CLI::Registry
+
+      # List command
+      class List < Dry::CLI::Command
+        desc "List profiles"
+
+        def call(*)
+          # Check if '.hub_credentials' file exist and contains credentials.
+          hub_credentials_file = Hub::Profile::Util.hub_credentials_file
+          hub_profiles = Hub::Profile::Util.load_hub_credentials_file(hub_credentials_file)
+          hub_profiles.each_key { |p| puts p.to_s }
+        rescue TomlRB::ParseError, Dry::Struct::Error => e
+          # TomlRB::ParseError
+          # Dry::Struct::Error
+          puts "Error: Unable to parse credentials\n#{e.message}"
+        rescue SystemCallError, IOError, TomlRB::ParseError => e
+          # Errno::ENOENT - Not such file or directory.
+          # Errno::EACCES - Permission denied.
+          puts "Error: #{e.message}"
+        end
+      end
 
       # Version command.
       class Version < Dry::CLI::Command
