2275f09f047568bf7f2e2d7cb83d8831ebe23b71
Alberto Colon Viera <aacv@alberti.co>
2020-08-13 00:26:58 -0400

    Modifying Hub::Profile::Util class.  * Requiring Hub::Profile::Host.  * Requiring SafeYAML and enabling :safe loading.  * Defining new constants: HUB_CMD and HUB_VERSION_CMD.  * New method: parse_hub_config() - Method to parse and convert YAML from    the hub config file into a Hash with Hub::Profile::Host instances.  * New method: load_hub_config_file() - Method to read the hub config file    and parse its YAML content.  * New method: dump_hub_config() - Method to convert the Hash with    Hub::Profile::Host instances into valid YAML format as compliant with    the hub command line tool.  * New method: dump_hub_config_to_file() - Method to write the hub config    in memory to the hub config file in YAML format.  * New method: dump_hub_credentials() - Method to convert the Hash with    Hub::Profile::Credentials instances into valid TOML format.  * New method: dump_hub_credentials_to_file() - Method to write the hub    credentials in memory to the hub credentials file in TOML format.  * New method: host_is_contained_in_hub_credentials? - Method to determine    if a hub config host entry in memory is contained within the hub credentials    in memory.  * Minor changes to parse_hub_credentials() and load_hub_credentials_file().

--- lib/hub/profile/util.rb
+++ lib/hub/profile/util.rb
@@ -1,14 +1,21 @@
 # frozen_string_literal: true
 
 require "hub/profile/credentials"
+require "hub/profile/host"
 require "toml-rb"
+require "safe_yaml"
 
 module Hub
   module Profile
     # Class with utility/helper methods.
     module Util
+      # Constants.
       HUB_CONFIG_FILENAME = "hub"
       HUB_CREDENTIALS_FILENAME = ".hub_credentials"
+      HUB_CMD = "hub"
+      HUB_VERSION_CMD = "hub version"
+      # Configure SafeYAML.
+      SafeYAML::OPTIONS[:default_mode] = :safe
 
       # As per hub's documentation, the configuration file path is first determined by
       # the HUB_CONFIG environment variable. Else, if $XDG_CONFIG_HOME is present, the
@@ -25,6 +32,34 @@ module Hub
         end
       end
 
+      def parse_hub_config(hub_config)
+        hosts = {}
+        hub_config.each do |d, h|
+          hosts[d] = Hub::Profile::Host.new(h[0])
+        end
+        hosts
+      end
+
+      def load_hub_config_file(file)
+        # Load file.
+        data = YAML.load_file(file)
+        Hub::Profile::Util.parse_hub_config(data)
+      end
+
+      def dump_hub_config(hub_config)
+        # TODO: Validate schema.
+        hosts = {}
+        hub_config.each do |d, h|
+          hosts[d] = [h]
+        end
+        YAML.dump(hosts)
+      end
+
+      def dump_hub_config_to_file(hub_config, file)
+        data_yaml = Hub::Profile::Util.dump_hub_config(hub_config)
+        TTY::File.create_file(file, data_yaml, force: true, verbose: false)
+      end
+
       # Keeping the .hub_credentials file in the same path as hub's config.
       def hub_credentials_file
         if ENV["HUB_CONFIG"]
@@ -39,22 +74,46 @@ module Hub
         end
       end
 
+      def parse_hub_credentials(hub_credentials)
+        profiles = {}
+        hub_credentials.each do |p, c|
+          profiles[p] = Hub::Profile::Credentials.new(c)
+        end
+        profiles
+      end
+
       def load_hub_credentials_file(file)
         # Load file.
-        c = TomlRB.load_file(file)
-        Hub::Profile::Util.parse_hub_credentials(c)
+        data = TomlRB.load_file(file)
+        Hub::Profile::Util.parse_hub_credentials(data)
       end
 
-      def parse_hub_credentials(hub_credentials)
-        hub_profiles = {}
+      def dump_hub_credentials(hub_credentials)
+        # TODO: Validate schema.
+        profiles = {}
         hub_credentials.each do |p, c|
-          hub_profiles[p] = Hub::Profile::Credentials.new(c)
+          profiles[p] = c.to_hash
         end
-        hub_profiles
+        TomlRB.dump(profiles)
       end
 
-      module_function :hub_config_file, :hub_credentials_file,
-                      :load_hub_credentials_file, :parse_hub_credentials
+      def dump_hub_credentials_to_file(hub_credentials, file)
+        data_toml = Hub::Profile::Util.dump_hub_credentials(hub_credentials)
+        TTY::File.create_file(file, data_toml, force: true, verbose: false)
+      end
+
+      def host_is_contained_in_hub_credentials?(domain, host, hub_credentials)
+        profile = hub_credentials.select do |_, c|
+          c.host == domain && c.user == host.user && c.oauth_token == host.oauth_token && c.protocol == host.protocol
+        end
+        !profile.empty?
+      end
+
+      module_function :hub_config_file, :parse_hub_config, :load_hub_config_file,
+                      :dump_hub_config, :dump_hub_config_to_file,
+                      :hub_credentials_file, :parse_hub_credentials, :load_hub_credentials_file,
+                      :dump_hub_credentials, :dump_hub_credentials_to_file,
+                      :host_is_contained_in_hub_credentials?
     end
   end
 end
