4f16ec366f23b8d2a2db6065faec2bdef2f3b48f
Alberto Colon Viera <aacv@alberti.co>
2020-08-13 00:34:20 -0400

    Modifying Hub::Profile::CLI classes to include Hub::Profile::CLI::Command.

--- lib/hub/profile/cli.rb
+++ lib/hub/profile/cli.rb
@@ -1,11 +1,7 @@
 # frozen_string_literal: true
 
-require "hub/profile/version"
-require "hub/profile/util"
+require "hub/profile/cli/command"
 require "dry/cli"
-require "tty/which"
-require "tty/command"
-require "tty/prompt"
 
 module Hub
   module Profile
@@ -15,41 +11,38 @@ module Hub
 
       # List command.
       class List < Dry::CLI::Command
+        include Hub::Profile::CLI::Command
+
         desc "List profiles"
 
         def call(*)
           prompt = TTY::Prompt.new
           # Load '.hub_credentials' file.
           hub_credentials_file = Hub::Profile::Util.hub_credentials_file
-          hub_profiles = Hub::Profile::Util.load_hub_credentials_file(hub_credentials_file)
+          hub_credentials = Hub::Profile::Util.load_hub_credentials_file(hub_credentials_file)
           # Print profiles found in '.hub_credentials'.
-          hub_profiles.each_key { |p| prompt.say p.to_s }
+          print_profiles_in_hub_credentials(prompt, hub_credentials)
         rescue TomlRB::ParseError, Dry::Struct::Error => e
           # TomlRB::ParseError - Expected if an error occurs when parsing '.hub_credentials'.
           # Dry::Struct::Error - Expected if attributes from a profile don't match the credentials schema.
-          prompt.say "Error: Unable to parse hub credentials\n#{e.message}"
-          exit 1
+          print_hub_credentials_parse_error_and_exit(prompt, e)
         rescue SystemCallError, IOError, TomlRB::ParseError => e
           # Errno::ENOENT - Not such file or directory.
           # Errno::EACCES - Permission denied.
-          prompt.say "Error: #{e.message}"
-          exit 1
+          print_error_and_exit(prompt, e)
         end
       end
 
       # Version command.
       class Version < Dry::CLI::Command
+        include Hub::Profile::CLI::Command
+
         desc "Print version"
 
         def call(*)
           prompt = TTY::Prompt.new
-          if TTY::Which.exist?("hub")
-            cmd = TTY::Command.new(:printer => :quiet)
-            cmd.run("hub version")
-          else
-            prompt.say "Error: hub command not found"
-          end
-          prompt.say "hub-profile version #{Hub::Profile::VERSION}"
+          print_hub_version(prompt)
+          print_hub_profile_version(prompt)
         end
       end
     end
